7cd84623c56b7bcb8605c4a06ef185ddcf95e565
Davide Chirichella <davidechirichella10@gmail.com>
2026-01-11 17:01:12 +0100

    fixed: N in tests

--- slurm/mpi_strong_benchmark.sh
+++ slurm/mpi_strong_benchmark.sh
@@ -22,7 +22,7 @@ mkdir -p $RESULTS_DIR
 echo "Run,P,N,Time" > $RESULTS_DIR/strong_mpi_results_${EXCHANGE_MODE}.csv
 
 for P in 1 2 4 8 12 16 20 24; do
-    for i in {1..10}; do
+    for i in {1..3}; do
         RESULT=$(srun -n $P $EXEC $N_FIXED $SEED $EXCHANGE_MODE -b --q)
         echo "$i,$RESULT" >> $RESULTS_DIR/strong_mpi_results_${EXCHANGE_MODE}.csv
     done
--- slurm/mpi_weak_benchmark.sh
+++ slurm/mpi_weak_benchmark.sh
@@ -18,14 +18,14 @@ RESULTS_DIR="../results"
 mkdir -p $RESULTS_DIR
 
 TASKS=(1     2     4     8     12    16    20   24)
-SIZES=(5000 7071 10000 14142 17385 20000 22360 24500)
+SIZES=(5000 7071 10000 14142 17320 20000 22360 24494)
 
 echo "Run,P,N,Time" > $RESULTS_DIR/weak_mpi_results_${EXCHANGE_MODE}.csv
 
 for i in "${!TASKS[@]}"; do
     P=${TASKS[$i]}
     N=${SIZES[$i]}
-    for run in {1..10}; do
+    for run in {1..3}; do
         RESULT=$(srun -n $P $EXEC $N $SEED $EXCHANGE_MODE -b --q)
         echo "$run,$RESULT" >> $RESULTS_DIR/weak_mpi_results_${EXCHANGE_MODE}.csv
     done
