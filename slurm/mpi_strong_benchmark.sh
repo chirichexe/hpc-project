482821fac8b724ed0c26c30661c1484b3e960596
Davide Chirichella <davidechirichella10@gmail.com>
2026-01-12 23:25:32 +0100

    fixed: cpu mpi params

--- slurm/mpi_strong_benchmark.sh
+++ slurm/mpi_strong_benchmark.sh
@@ -4,9 +4,7 @@
 #SBATCH -t 00:20:00
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=48
-#SBATCH --cpus-per-task=1
-#SBATCH --exclusive
-#SBATCH -o strong_scaling.out
+#SBATCH -o strong_scaling_mpi.out
 
 EXCHANGE_MODE=$1
 
@@ -26,8 +24,7 @@ echo "Run,P,N,Time" > $RESULTS_DIR/strong_mpi_results_${EXCHANGE_MODE}.csv
 # Ciclo sul numero di processi
 for P in 1 2 4 8 12 16 20 24 48 ; do
     for i in {1..3}; do
-        # Esecuzione: usiamo --exact per mappare correttamente i task sui core
-        RESULT=$(srun -n $P --exact --cpus-per-task=1 $EXEC $N $SEED $EXCHANGE_MODE -b -q)
+        RESULT=$(srun -n $P $EXEC $N $SEED $EXCHANGE_MODE -b -q)
         echo "$i,$RESULT" >> $RESULTS_DIR/strong_mpi_results_${EXCHANGE_MODE}.csv
     done
 done
