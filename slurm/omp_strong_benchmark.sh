42e8f9c947f9b7d245ae30f85c9f6f1296d92072
Davide Chirichella <davidechirichella10@gmail.com>
2026-01-18 11:45:49 +0100

    added srun on openmp

--- slurm/omp_strong_benchmark.sh
+++ slurm/omp_strong_benchmark.sh
@@ -7,22 +7,22 @@
 #SBATCH --cpus-per-task=48
 #SBATCH -o strong_scaling_omp.out
 
-
-EXEC="./bin/binarize_omp"
+EXEC="srun ./bin/binarize_omp"
 SEED=1234
 N=10000
 RESULTS_DIR="./results"
 
 mkdir -p $RESULTS_DIR
 
-echo "Run,P,N,Time" > $RESULTS_DIR/strong_omp_results.csv
+echo "Run,P,N,Time" >$RESULTS_DIR/strong_omp_results.csv
 
 # Ciclo sul numero di thread
 for T in 1 2 4 8 12 16 20 24 48; do
-    export OMP_NUM_THREADS=$T
-    for i in {1..3}; do
-        # Esecuzione: passiamo T come primo argomento dell'eseguibile
-        RESULT=$($EXEC $T $N $SEED -b -q)
-        echo "$i,$RESULT" >> $RESULTS_DIR/strong_omp_results.csv
-    done
+  export OMP_NUM_THREADS=$T
+  for i in {1..3}; do
+    # Esecuzione: passiamo T come primo argomento dell'eseguibile
+    RESULT=$($EXEC $T $N $SEED -b -q)
+    echo "$i,$RESULT" >>$RESULTS_DIR/strong_omp_results.csv
+  done
 done
+
--- slurm/omp_weak_benchmark.sh
+++ slurm/omp_weak_benchmark.sh
@@ -7,25 +7,26 @@
 #SBATCH --cpus-per-task=48
 #SBATCH -o weak_scaling_omp.out
 
-EXEC="./bin/binarize_omp"
+EXEC="srun ./bin/binarize_omp"
 SEED=1234
 RESULTS_DIR="./results"
 
 mkdir -p $RESULTS_DIR
 
 # Numero di thread e dimensioni N corrispondenti (N_base * sqrt(T))
-THREADS=(1     2     4     8     12    16    20    24    48)
+THREADS=(1 2 4 8 12 16 20 24 48)
 SIZES=(5000 7071 10000 14142 17320 20000 22360 24494 34641)
 
-echo "Run,P,N,Time" > $RESULTS_DIR/weak_omp_results.csv
+echo "Run,P,N,Time" >$RESULTS_DIR/weak_omp_results.csv
 
 for i in "${!THREADS[@]}"; do
-    T=${THREADS[$i]}
-    N=${SIZES[$i]}
-    export OMP_NUM_THREADS=$T
-    
-    for run in {1..3}; do
-        RESULT=$($EXEC $T $N $SEED -b -q)
-        echo "$run,$RESULT" >> $RESULTS_DIR/weak_omp_results.csv
-    done
+  T=${THREADS[$i]}
+  N=${SIZES[$i]}
+  export OMP_NUM_THREADS=$T
+
+  for run in {1..3}; do
+    RESULT=$($EXEC $T $N $SEED -b -q)
+    echo "$run,$RESULT" >>$RESULTS_DIR/weak_omp_results.csv
+  done
 done
+
